d62e12e31a1b44b0238a92e41d0bb4b1f3137dc7
Dmytro Vasyliuk <dmytro.vasyliuk1@gmail.com>
2022-03-16 12:12:40 +0200

    Test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,14 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, FormGroupDirective, NgForm, Validators } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+
+/** Error when invalid control is dirty, touched, or submitted. */
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +16,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Test';
+  public editForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,) {
+    this.editForm = this.fb.group({
+      name: new FormControl('', Validators.required),
+      role: new FormControl('', Validators.required),
+      newPassword: new FormControl('', Validators.required),
+      repeatPassword: new FormControl('', Validators.required),
+    });
+  }
+
+  getErrorMessage() {
+    if (this.editForm.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.editForm.hasError('email') ? 'Not a valid email' : '';
+  }
+  onSubmitForm(){
+    console.log(12);
+    
+  }
 }
